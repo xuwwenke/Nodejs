07af556f0afce9c3c804de1c25e1413156869ace
xuwwenke <3492517331@qq.com>
2019-09-08 21:45:48 +0800

    hello

--- 03-console/01-format.js
+++ 03-console/01-format.js
@@ -1,2 +1,21 @@
 #!/usr/bin/node
 console.log('hello world!');
+
+const user={
+  name:'王顶',
+  age:41,
+  qq:'408542507'
+};
+const log=console.log;
+
+//三种占位符
+log('name:%s',user.name);
+log('age:%d',user.age);
+log('JSON:%j',user);
+
+log('qq:%s',user.qq);
+log('qq:',user.qq);
+log('qq:'+user.qq);
+log(`qq:${user.qq}`);
+
+console.error('Error!something wrong!');
--- 03-console/02-time.js
+++ 03-console/02-time.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
-console.log('hello world!');
+
+console.time('TEST'); 
+
+longTask();
+
+console.timeEnd('TEST');
+function longTask() {
+  var n;
+  for(var i=0; i<10000; i++) {
+    for(var j=0; j<10000; j++) {
+      n = i * j;
+    }
+  }
+  return n;
+}
