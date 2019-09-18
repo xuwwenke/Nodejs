5270f020c44e8f20983c89105d6a61075661ca24
xuwwenke <3492517331@qq.com>
2019-09-18 16:28:55 +0800

    hello

---
+++ 09-stream/05-transform.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+console.log('hello world!');
+
+const stdin  = process.stdin,
+      stdout = process.stdout,
+      Trans  = require('stream').Transform;
+stdin.setEncoding('utf8');
+var tf = new Trans();
+tf._transform = function(chunk, encoding, callback) {
+  this.push(Buffer(chunk.toString('utf8').toUpperCase()));
+  callback();
+};
+stdin.pipe(tf).pipe(stdout);
