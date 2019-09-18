b24f83bdfd3692eea267cdcd5a8476c20857773e
xuwwenke <3492517331@qq.com>
2019-09-18 16:21:33 +0800

    hello

--- 10-file-system/01-my-cat-mix.js
+++ 10-file-system/01-my-cat-mix.js
@@ -5,8 +5,12 @@ const log=console.log,
       fs=require('fs'),
       file=process.argv[2]||__filename;
 
-const fid=fs.openSync(file,'r');
-
-log(fs.readFileSync(fid).toString('utf8'));
-
-fs.closeSync(fid);
+try{
+  const fid=fs.openSync(file,'r');
+  log(fs.readFileSync(fid).toString('utf8'));
+  fs.closeSync(fid);
+}catch(e){
+  console.error(e.message);
+  process.exit(1);
+}
+//fs.closeSync(fid);
