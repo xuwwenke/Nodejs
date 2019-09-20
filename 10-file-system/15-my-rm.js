9442ef74d224f975d9c6392e17f3e3f36828c1bb
xuwwenke <3492517331@qq.com>
2019-09-20 16:40:04 +0800

    hello

--- 10-file-system/15-my-rm.js
+++ 10-file-system/15-my-rm.js
@@ -3,9 +3,29 @@ console.log('hello world!');
 
 const log=console.log,
       fs=require('fs'),
+      join=require('path').join,
       dir=process.argv[2];
 
 if(typeof(dir)==='undefined'){
-  log('存在错误');
+  console.error('命令行出现错误，命令行参数不存在！');
+  process.exit(1);
 }
+if(!fs.existsSync(src)){
+  consloe.error('%s not exists',src);
+  process.exit(1);
+}
+if(fs.statSync(src).isFile()) fs.unlinkSync(src);
+if(fs.statSync(src).isDirectory()) deleteDir(src);
+function delete(folder){
+  var files=fs.readirSync(folder);
 
+  for(var i=0;i<files.length;i++){
+    var file=join(folder,files[i]);
+    if(fs.statSync(file).isFinite()){
+      fs.unlinkSync(file);
+      continue;
+    }
+    if(fs.statSync(file).isDirectory()) deleteDir(file);
+  }
+  fs.rmdirSync(folder);
+}
